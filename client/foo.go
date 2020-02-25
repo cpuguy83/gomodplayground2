9af0a6c32d88a493a8ae6b8d5fa23369a0975869
Brian Goff <cpuguy83@gmail.com>
2020-02-25 12:23:10 -0800

    Remove main module

--- client/foo.go
+++ client/foo.go
@@ -1,7 +1,5 @@
 package client
 
-import _ "github.com/cpuguy83/gomodplayground"
-
 func Something() string {
 	return "something"
 }
--- client/go.mod
+++ client/go.mod
@@ -1,5 +1,3 @@
 module github.com/cpuguy83/gomodplayground/client
 
 go 1.13
-
-require github.com/cpuguy83/gomodplayground v0.1.3
